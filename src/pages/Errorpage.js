72e1b579512036cf0f7d7116c5db31f8a0d605ca
Mended-General <138323376+Mended-General@users.noreply.github.com>
2023-07-28 06:10:01 -0400

    added the error page styling

--- src/pages/Errorpage.js
+++ src/pages/Errorpage.js
@@ -1,3 +1,9 @@
+import style from "./Errorpage.module.css";
+
 export default function Errorpage() {
-  return <div></div>;
+  return (
+    <div className={style.overall}>
+      <h1 className={style.text}>404 - Page Not Found</h1>
+    </div>
+  );
 }
